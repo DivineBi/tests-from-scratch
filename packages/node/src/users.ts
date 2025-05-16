88d2d2db37a9fd9faeecb0920079b4585443eb9c
Divine-Bi <dbingila@outlook.com>
2025-05-16 14:59:49 +0200

    Lab 1 – Créer et tester une API Users en TypeScript (Express + Jest + Supertest)
    $ jest --coverage
     PASS  __tests__/users.test.ts
      API Users – TDD complet
        √ GET /users → 200 & [] (26 ms)
        √ POST /users sans name → 400 & { error } (25 ms)
        √ POST /users avec name → 201 & nouvel user (4 ms)
        √ GET /users après création → liste avec Alice (8 ms)
    
    ---------------|---------|----------|---------|---------|-------------------
    File           | % Stmts | % Branch | % Funcs | % Lines | Uncovered Line #s
    ---------------|---------|----------|---------|---------|-------------------
    All files      |    90.9 |       50 |   83.33 |    90.9 |
     server.ts     |   66.66 |        0 |       0 |   66.66 | 11-13
     users.ts      |     100 |      100 |     100 |     100 |
     usersStore.ts |     100 |      100 |     100 |     100 |
    ---------------|---------|----------|---------|---------|-------------------
    Jest: global coverage threshold for branches (85%) not met: 50%
    Jest: global coverage threshold for functions (90%) not met: 83.33%
    Test Suites: 1 passed, 1 total
    Tests:       4 passed, 4 total
    Snapshots:   0 total
    Time:        2.829 s
    Ran all test suites.

---
+++ packages/node/src/users.ts
@@ -0,0 +1,32 @@
+// src/users.ts
+
+import { Router, Request, Response } from "express";
+import { getUsers, createUser } from './usersStore';
+
+
+const router = Router();
+
+/** 
+ * GET /users 
+ * → renvoie la liste JSON des utilisateurs. 
+ */
+router.get('/', (_req, res) => { 
+    res.status(200).json(getUsers());
+});
+
+/** 
+ * POST /users 
+ * Body attendu: { name: string } 
+ * → crée un utilisateur ou renvoie 400 si name manquant. 
+ */
+router.post("/", (req: Request, res: Response) => {
+	const { name } = req.body
+	if (!name) {
+		res.status(400).json({ error: "name is required" })
+        return
+	}
+	const user = createUser(name)
+	res.status(201).json(user)
+})
+
+export default router;
